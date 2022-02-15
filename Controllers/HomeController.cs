5cc61d020803d528e42f684717578387ed5349c0
Simon Roger Müller <88650413+Appli4life@users.noreply.github.com>
2022-02-15 16:23:41 +0100

    Home Conttroller Bearbeiter undo create

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,32 +10,19 @@ namespace Ski_Service_Applikation.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        { 
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult About()
         {
-
             using (var context = new ski_serviceEntities())
             {
+  
                 List<angebot> Angebote = context.angebot.ToList();
 
                 var first = Angebote.First();
-
-                angebot a = new angebot()
-                {
-                    Kategorie_ID = 2,
-                    Marke_ID = 2,
-                    Preis_pro_Tag = 22.2
-                };
-
-                context.angebot.Add(a);
-
-                context.SaveChanges();
-
 
                 var ViewModel = new firstAngebotViewModel()
                 {
